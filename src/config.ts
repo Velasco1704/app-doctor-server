f86acda191c7f51076bb7a33033f24440e40c33f
Daniel Velasco <danilovelga4747@gmail.com>
2023-05-26 14:16:10 -0500

    a little change

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT_APP;
+export const PORT = process.env.PORT;
 export const JWT_SECRET = process.env.JWT_SECRET || "";
